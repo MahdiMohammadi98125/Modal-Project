400e84de76754665ce00d8f1e3b822acbdd6a261
MahdiMohammadi98125 <109587937+MahdiMohammadi98125@users.noreply.github.com>
2022-12-23 10:36:52 -0800

    the needed elements stored in the variable

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
