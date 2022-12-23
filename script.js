ed7ac022492f7106661971c957f8e5c60dec4afb
MahdiMohammadi98125 <109587937+MahdiMohammadi98125@users.noreply.github.com>
2022-12-23 11:08:55 -0800

    working with classed

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
+
+// open the modal window
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.style.display = 'block';
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// close the modal window
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.style.display = 'none';
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
